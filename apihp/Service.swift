c37cab9b374b9ef69f470befc7ceab60d200903b
mrpedroox <pedrohenriquetavares06@gmail.com>
2026-04-28 19:16:19 -0300

    arquivos para mostrar e consumir a api

---
+++ apihp/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  apihp
+//
+//  Created by Turma01-17 on 28/04/26.
+//
+
+import Foundation
+import Combine
+
+struct Service{
+    func fetchHaPo(url: URL) -> AnyPublisher<[Hapo], Error>{
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .map(\.data)
+            .decode(type: [Hapo].self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+    }
+}
