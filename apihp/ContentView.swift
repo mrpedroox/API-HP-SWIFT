599400dc906326d8ffcd2aa80f98e27e531e7c7c
mrpedroox <pedrohenriquetavares06@gmail.com>
2026-04-28 18:41:38 -0300

    testando mds

---
+++ apihp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  apihp
+//
+//  Created by Turma01-17 on 28/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ apihp/apihpApp.swift
@@ -0,0 +1,17 @@
+//
+//  apihpApp.swift
+//  apihp
+//
+//  Created by Turma01-17 on 28/04/26.
+//
+
+import SwiftUI
+
+@main
+struct apihpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
